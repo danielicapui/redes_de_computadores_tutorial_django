69fa2252bef108553861b4b105dbcdfa0e77faf1
danielicapui <daniel.icapui@hotmail.com>
2023-10-09 10:14:49 -0300

    #6 melhorado admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from .models import Question,Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 #admin.site.register(Question)
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("id","question_text","pub_date")
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ("id","question_text","pub_date","was_published_recently")
     search_fields=("id","question_text","pub_date")
-
+    list_filter = ["pub_date"]
 #@admin.site.register(Choice)
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ("id","question","choice_text","votes")
     search_fields=("id","question","choice_text","votes")
+    
